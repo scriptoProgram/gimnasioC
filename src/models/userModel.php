f21d94f7c52a6cb441be9e31b77a312c320a3f02
scriptoProgram <fabianmejia828@gmail.com>
2025-02-23 21:53:10 -0600

    c-log

--- src/models/userModel.php
+++ src/models/userModel.php
@@ -1,12 +1,20 @@
 <?php
-    require_once './../config/database.php';
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+    require_once realpath(__DIR__ . '/../config/database.php');
 
     class UserModel {
         public static function getUserByEmail($email) {
             $conn = Database::Connect();
-            $stmt = $conn->prepare("SELECT id_empleado AS id, pass, 'empleado' AS tipo FROM empleado WHERE correo = ?");
-                            //    UNION 
-                            //    SELECT id_miembro AS id, pass, 'miembro' AS tipo FROM miembro WHERE correo = ?");
+            if ($conn === null) {
+                die("Error de conexión a la base de datos.");
+            }
+
+            // $stmt = $conn->prepare("SELECT id_empleado AS id, pass, tipo_empleado AS tipo FROM empleado WHERE correo = ?");
+            //                 //    UNION 
+            //                 //    SELECT id_miembro AS id, pass, 'miembro' AS tipo FROM miembro WHERE correo = ?");
+            $stmt = $conn->prepare("SELECT id_empleado AS id, pass, tipo_empleado AS tipo FROM empleado WHERE correo = ?");
             // $stmt->execute(['email' => $email, 'password' => $password]);
             $stmt->execute([$email]);
             return $stmt->fetch();
