ffe5c1487408d873769bf4844b4cd541e82ebfe1
scriptoProgram <fabianmejia828@gmail.com>
2025-02-23 21:27:47 -0600

    c-login DIR

--- src/controllers/authController.php
+++ src/controllers/authController.php
@@ -1,5 +1,5 @@
 <?php
-    require_once './../models/userModel.php';
+    require_once __DIR__ . './../models/userModel.php';
     require_once './../helpers/sessionHelper.php';
 
     class AuthController {
